f677f9ea6bae45f6ee96895dc953b1b96f5b994f
yusefoliver <isaiahyoliver@gmail.com>
2025-05-06 20:12:00 -0500

    Add State to track the dice roll action

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [diceRoll, setDiceRoll] = useState(1);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app</Text>
