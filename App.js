6a4c920287b936848c70a372fc394f1978b10bf2
yusefoliver <isaiahyoliver@gmail.com>
2025-05-06 20:20:57 -0500

    update imports for useState

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const [diceRoll, setDiceRoll] = useState(1);
-  
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app</Text>
