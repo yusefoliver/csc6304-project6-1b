271c19d8fe06ee86c5867b5c2be6f3f21dc0c2b1
yusefoliver <isaiahyoliver@gmail.com>
2025-05-06 20:25:34 -0500

    Add roll dice function

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   const [diceRoll, setDiceRoll] = useState(1);
 
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    setDiceRoll(randomNumber);
+  };
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app</Text>
